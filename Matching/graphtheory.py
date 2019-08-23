0d5184f5d9d8befa2e2ce32454089715583f62dc
Brandon Goodell <b-g-goodell@users.noreply.github.com>
2019-08-22 18:56:15 -0600

    Comments and tests completed

--- Matching/graphtheory.py
+++ Matching/graphtheory.py
@@ -12,10 +12,10 @@ class BipartiteGraph(object):
 
     Attributes:
         data        : arbitrary.
-        count       : integer.
+        count       : integer, used for naming new nodes
         left_nodes  : dictionary. "nodes side 0." keys & vals are node_idents (integer)
         right_nodes : dictionary. "nodes side 1." keys & vals are node_idents (integer)
-        blue_edges  : dictionary. "edges color 0." keys & vals are edge_idents (integer pairs)
+        blue_edges  : dictionary. "edges color 0." keys are edge_idents (integer pairs), vals are non-negative numbers.
         red_edges   : dictionary. "edges color 1." keys are edge_idents (integer pairs), vals are non-negative numbers.
 
     Functions (see below for comments)
@@ -25,11 +25,9 @@ class BipartiteGraph(object):
         del_node
         chk_colored_match
         check_colored_maximal_match
-        extend_match
-        boost_match
-        breadth_first_search
+        extend
+        boost
 
-    TODO: paths need to be checked to verify they aren't walks! Walks can return to a node or edge, paths can't.
     """
 
     def __init__(self, par=None):
@@ -65,8 +63,8 @@ class BipartiteGraph(object):
         self.blue_edges.update(par['blue_edges'])
 
     def add_node(self, b):
-        """ Take a bit b indicating side as input. Check that b is a bit and return None if it isn't. Otherwise, create
-        a new node on side b, outputting the node_ident.
+        """ Take a bit b indicating side as input (b = 0 are left nodes, b = 1 are right nodes). Check that b is a bit
+        and return None if it isn't. Otherwise, create a new node on side b, outputting the node_ident.
         """
         assert b in [0, 1]
         if b or not b:
@@ -92,10 +90,8 @@ class BipartiteGraph(object):
           eid[0] not in left_nodes  or
           eid[1] not in right_nodes
         Otherwise update edge with color b and edge id eid to have weight w. Note: this OVER-WRITES any previous
-        edge weight. Since we always add all red edges with weight 0.0, this allows us to add weights to edges that
-        exist in the graph, and assume weight 0.0 for edges that do not.
+        edge weight of color b with weight w and over-writes the weight of color 1-b with weight 0.0.
 
-        Allow non-zero weights only for one color at a time for an edge.
         """
         assert b in [0, 1]
         assert w >= 0.0
@@ -114,50 +110,61 @@ class BipartiteGraph(object):
         return result
 
     def del_edge(self, eid):
-        """ Take (x,y) as input (an edge_ident). If both of these are legitimate nodes, then we set edge weights in
-        both edge dictionaries to 0.0. If either node does not exist, then the edge entry is deleted from the dictionary
+        """ Take eid = (x, y) as input (an edge_ident). If both of these are nodes on the proper sides, then we set edge
+        weights in both edge dictionaries to 0.0. If either node does not exist or is on the wrong side, then the edge
+        entry is deleted from the dictionary. This always succeeds so we return True.
         """
         if eid[0] not in self.left_nodes or eid[1] not in self.right_nodes:
             if eid in self.blue_edges:
                 del self.blue_edges[eid]
             if eid in self.red_edges:
                 del self.red_edges[eid]
-            result = True
         else:
             self.blue_edges.update({eid: 0.0})
             self.red_edges.update({eid: 0.0})
-            result = True
-        return result
+        return True
 
     def del_node(self, x):
         """  Take x as input (a node ident).
         Fail (output False) if:
           x is not in left_nodes and
           x is not in right_nodes
-        Otherwise remove x from all node dictionaries and remove from both edge dictionaries all edge_idents of the
-          form (x,y) or (y,x) and output True.
+        Otherwise remove x from all node dictionaries and delete both color edge identities like (x, y) and (y, x).
+        This always succeeds so we return True.
         """
-        result = x in self.left_nodes or x in self.right_nodes
-        if result:
+        if x in self.left_nodes or x in self.right_nodes:
             if x in self.left_nodes:
                 del self.left_nodes[x]
             if x in self.right_nodes:
                 del self.right_nodes[x]
-            eids_to_remove = [eid for eid in list(self.blue_edges.keys()) if (eid[0] == x or eid[1] == x)]
-            eids_to_remove += [eid for eid in list(self.red_edges.keys()) if (eid[0] == x or eid[1] == x)]
-            for eid in eids_to_remove:
-                self.del_edge(eid)
-        return result
+
+        eids_to_remove = [eid for eid in list(self.blue_edges.keys()) if (eid[0] == x or eid[1] == x)]
+        eids_to_remove += [eid for eid in list(self.red_edges.keys()) if (eid[0] == x or eid[1] == x)]
+        for eid in eids_to_remove:
+            self.del_edge(eid)
+
+        return True
 
     def chk_colored_match(self, b, input_match):
+        """ Take as input a color b and an input_match. If input_match is empty, then it's a trivial match and we return
+        True. Otherwise, we check if input_match is a single-colored match with color b, returning True if so and
+        False otherwise.
+        """
         if len(input_match) == 0:
             result = True
         else:
+            # Every left vertex incident with an edge in the purported match must be incident with exactly one edge.
             left_vertices = [eid[0] for eid in input_match]
+            # If a left vertex is duplicated in this list, then two edges are incident with the same left node.
             dedupe_lefts = list(set(left_vertices))
+            # Every right vertex incident with an edge in the purported match must be incident with exactly one edge.
             right_vertices = [eid[1] for eid in input_match]
+            # If a right vertex is duplicated, then two edges are incident with the same right node.
             dedupe_rights = list(set(right_vertices))
-            vtx_disj = (len(dedupe_lefts)==len(dedupe_rights) and len(dedupe_lefts)==len(input_match))
+            # Therefore this check verifies that the edge set is pairwise vertex disjoint!
+            vtx_disj = (len(dedupe_lefts) == len(dedupe_rights) and len(dedupe_lefts) == len(input_match))
+            # Now we check that the match is all of one color, and that color is b.
+            correct_color = False
             if vtx_disj:
                 blue_wt = sum([self.blue_edges[eid] for eid in input_match])
                 red_wt = sum([self.red_edges[eid] for eid in input_match])
@@ -169,7 +176,7 @@ class BipartiteGraph(object):
     def check_colored_maximal_match(self, b, input_match):
         """  Take input_match as input (a list of edge_idents). Fail (output False) if:
             chk_colored_match(input_match) is False or
-            any pair of unmatched nodes has an edge with the same color as the edges in input_match with nonzero weight
+            there exists an edge with color b and non-zero weight not in input_match with unmatched endpoints
           Otherwise output True.
         """
 
@@ -184,27 +191,24 @@ class BipartiteGraph(object):
             unmatched_rights = [nid for nid in self.right_nodes if nid not in matched_rights]
             for x in unmatched_lefts:
                 for y in unmatched_rights:
-                    w = None
-                    if b and self.red_edges[(x, y)] > 0.0:
-                        w = self.red_edges[(x, y)]
-                    elif not b and self.blue_edges[(x, y)] > 0.0:
-                        w = self.blue_edges[(x, y)]
-                    if w is not None and w > 0.0:
+                    if (b and self.red_edges[(x, y)] > 0.0) or (not b and self.blue_edges[(x, y)] > 0.0):
                         # we found an edge that could increase match size!
                         result = False
         return result
 
     def _parse(self, b, input_match):
         """ parse takes a color b and input_match and either crashes because input_match isn't a match with color b
-        or returns the following sets for convenient usage in extend_match and boost_match
+        or returns the following lists for convenient usage in extend and boost.
             matched_lefts    :  left_nodes incident with an edge in input_match
             matched_rights   :  right_nodes incident with an edge in input_match
             unmatched_lefts  :  left_nodes not incident with any edge in input_match
             unmatched_rights :  right_nodes not incident with any edge in input_mach
             apparent_color   :  the observed color of input_match (which should match the input color b)
-            non_match_edges  :  edges with color b that are not in input_match
+            non_match_edges  :  edges with color b that are not in input_match, including zero-weight edges
         """
+        # Check input_match is a match of color b
         assert self.chk_colored_match(b, input_match)
+        assert b in [0, 1]
 
         # Node sets
         matched_lefts = [eid[0] for eid in input_match]
@@ -212,84 +216,57 @@ class BipartiteGraph(object):
         unmatched_lefts = [nid for nid in self.left_nodes if nid not in matched_lefts]
         unmatched_rights = [nid for nid in self.right_nodes if nid not in matched_rights]
 
-        # color
-        if len(input_match) > 0:
-            apparent_color = input_match[0] in self.red_edges  # extract color
-            assert apparent_color == b
-        else:
-            apparent_color = b
-
-        # non-match non-zero edges
-        if apparent_color:
-            non_match_edges = [eid for eid in self.red_edges if eid not in input_match and self.red_edges[eid] > 0.0]
-        elif not apparent_color:
-            non_match_edges = [eid for eid in self.blue_edges if eid not in input_match and self.blue_edges[eid] > 0.0]
-        else:
-            assert False
-
-        return (matched_lefts, matched_rights, unmatched_lefts, unmatched_rights, apparent_color, non_match_edges)
-
-    def _cleanup(self, b, shortest_paths, non_match_edges, input_match):
-        """ For each next_path in shortest_paths, compute :
-            (i)   the symmetric difference between next_path and input_match and
-            (ii)  the cumulative weight of this symmetric difference, and
-            (iii) the difference between the cumulative weight from input_match and the weight from (ii)
-          Order the list_of_all_shortest_paths by gain, discarding negative gain paths and discarding any paths with
-          length greater than the shortest path with positive gain. Construct a vertex-disjoint list of paths, say
-          v_d_list_of_paths, from the list_of_all_shortest_paths by greedy algorithm :
-            (i)   For the next_highest_gain_path in list_of_all_shortest_paths, check if next_highest_gain_path is
-                  vertex-disjoint from each other_path in v_d_list_of_paths.
-            (ii)  If so, include next_highest_gain_path in v_d_list_of_paths.
-            (iii) Go back to (i) lol
-          Compute the symmetric difference between each path in v_d_list_of_paths input_match.
-          Return the result.
-
-          An execution of cleanup takes as input a color, a list of paths, a list of non-match edges.
-
-        Note: cleanup could be written that does not take the non-match edges as input, which is unnecessary
-        to pass in, and can be conveniently extracted using the color b as in boost_match and extend_match):
-
-        However, we use this data (matched_lefts, matched_rights, unmatched_lefts, unmatched_rights, non_match_edges)
-        while executing both extend_match and boost_match, and these are the only functions that call cleanup.
-
-        """
-        # TODO: Special case: when shortest_paths is an empty list, the input_match is already optimal.
-        ordered_shortest_paths = []
+        # non-match edges
         if b:
-            edge_set = self.red_edges
-        elif not b:
-            edge_set = self.blue_edges
+            non_match_edges = [eid for eid in self.red_edges if eid not in input_match]
         else:
-            assert False
+            non_match_edges = [eid for eid in self.blue_edges if eid not in input_match]
 
-        # Sort by gain in decreasing order (greedy)
-        ordered_shortest_paths = sorted(shortest_paths, key=lambda z: z[1], reverse=True)
+        result = (matched_lefts, matched_rights, unmatched_lefts, unmatched_rights, b, non_match_edges)
+        return result
 
-        # Construct a list of vertex-disjoint paths from ordered_shortest_paths
-        v_d_list = []
-        touched_nodes = []
-        for next_pair in ordered_shortest_paths:
-            (next_path, gain) = next_pair
-            if len(v_d_list) == 0:
-                v_d_list = [next_path]
-                touched_nodes += [eid[0] for eid in next_path] + [eid[1] for eid in next_path]
-            else:
-                good_path = True
-                for eid in next_path:
-                    if eid[0] in touched_nodes or eid[1] in touched_nodes:
-                        good_path = False
-                        break
-                if good_path:
-                    v_d_list += [next_path]
-                    touched_nodes += [eid[0] for eid in next_path] + [eid[1] for eid in next_path]
-
-        # Iteratively take symmetric differences
+    def _clean(self, b, shortest_paths_with_gains, input_match):
+        """ Returns the input_match (when input shortest_paths is empty) or the iterative symdif of input_match """
+        assert b in [0, 1]
         result = input_match
-        if len(v_d_list) > 0:
-            for next_path in v_d_list:
-                temp = [eid for eid in result if eid not in next_path]
-                temp += [eid for eid in next_path if eid not in result]
-                result = temp
+        if len(shortest_paths_with_gains) > 0:
+            # Order the list
+            ordered_shortest_paths = sorted(shortest_paths_with_gains, key=lambda z: z[1], reverse=True)
+            # Construct vertex-disjoint list greedily
+            vd = []
+            touched_nodes = []
+            for path_and_gain in ordered_shortest_paths:
+                (next_path, gain) = path_and_gain
+                if gain > 0.0:
+                    # Collect some info for convenience
+                    first_edge = next_path[0]
+                    first_node = first_edge[0]
+                    last_edge = next_path[-1]
+                    p = len(next_path) % 2  # path parity
+                    assert p in [0, 1]
+                    last_node = last_edge[p]
+                    num_distinct_edges = len(list(set(next_path)))  # count distinct edges
+                    # collect sequence of nodes
+                    temp = []
+                    for i in range(len(next_path)):
+                        eid = next_path[i]
+                        idx_p = i % 2  # index parity
+                        temp += [eid[idx_p]]
+                    temp += [eid[1 - idx_p]]
+                    distinct_nodes = list(set(temp))
+                    num_distinct_nodes = len(distinct_nodes)  # count distinct vertices
+
+                    path_is_disj = (len([nid for nid in distinct_nodes if nid in touched_nodes]) == 0)
+                    if path_is_disj:
+                        vd += [next_path]
+                        touched_nodes += distinct_nodes
+
+            # Iteratively take symmetric differences
+            if len(vd) > 0:
+                for next_path in vd:
+                    temp = [eid for eid in result if eid not in next_path]
+                    temp += [eid for eid in next_path if eid not in result]
+                    result = temp
         return result
 
     def extend(self, b, input_match):
@@ -379,7 +356,7 @@ class BipartiteGraph(object):
                 assert len(shortest_paths) == 0 or found
                 if len(shortest_paths) > 0:
                     # print(shortest_paths)
-                    result = self._cleanup(b, shortest_paths, non_match_edges, input_match)
+                    result = self._clean(b, shortest_paths, input_match)
                 else:
                     result = input_match
         return result
@@ -488,7 +465,7 @@ class BipartiteGraph(object):
                     # in this case, length is not None and len(next_path) > length, so we discard next_path
                     pass
 
-            result = self._cleanup(b, shortest_cycles_with_pos_gain, non_match_edges, input_match)
+            result = self._clean(b, shortest_cycles_with_pos_gain, input_match)
         return result
 
     def optimize(self, b):
